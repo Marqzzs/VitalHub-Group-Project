a48f9379a4778998b93025cce0df4d83871702ac
Maurício Marques Pereira <55358961879@info.local>
2024-03-22 10:16:40 -0300

    subindo o projeto para o git

---
+++ vitalHub/src/services/Service.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+// declarar a porta da api
+
+const portaApi = '4466'
+
+//declarar o ip da maquina
+
+const ip = '172.16.39.98'
+
+//definir a base da url de acesso da api
+
+const apiUrlLocal = `http://${ip}:${portaApi}/api`
+
+// configuracao do axios
+
+export const api = axios.create({
+    baseURL: apiUrlLocal
+});
